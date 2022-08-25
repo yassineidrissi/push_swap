2d99cd5c4377f2a4d40fb735aca08133c3d2ed9e
yassine idrissi <yassine42idrissi@gmail.com>
2022-08-25 14:42:26 +0100

    the ft_sort_radix work but still errors in orders

--- outils/sort_lst_radix.c
+++ outils/sort_lst_radix.c
@@ -6,7 +6,7 @@
 /*   By: yaidriss <yaidriss@student1337.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/24 18:25:30 by yaidriss          #+#    #+#             */
-/*   Updated: 2022/08/25 13:26:27 by yaidriss         ###   ########.fr       */
+/*   Updated: 2022/08/25 14:41:38 by yaidriss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,31 +41,41 @@ int ft_lst_max(t_node **a)
 	}
 	return (max);
 }
+
+//? the last thing to do is to check how this function work
+
 void	ft_sort_radix(t_node **a, t_node **b)
 {
 	int	max_pos;
 	int i;
+	int j;
 	t_node **lst;
 	// elements_count= max_pos;
 
 	i = 0;
+	j = 1;
 	lst = malloc(sizeof(t_node *));
-	*lst = *a;
 	max_pos = ft_pos(ft_lst_max(a));
-	while (max_pos > 0)
+	printf("max pos is %d",max_pos);
+	while (max_pos >= j)
 	{
-		i = 0;
-		while(i < ft_lstsize(*a))
+		i = -1;
+		*lst = *b;
+		while(++i < ft_lstsize(*a))
 		{
-			if(((*a)->index & max_pos--) == 1)
+			if(((*a)->index & j) == 1)
 				pb(a, b);
 			else
 				ra(a);
-			i++;
+			
 		}
+		j++;
 		while (*lst)
 		{
-			pa(a, b);
+			if(((*lst)->index & j) == 1)
+				pa(a, b);
+			else
+				rb(a);
 			*lst = (*lst)->link;
 		}
 	}
